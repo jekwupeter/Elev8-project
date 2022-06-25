349f7e0ca4a6e861729f637627880afef524a5b7
UmokeTony <kachiblaise@gmail.com>
2022-06-25 22:34:57 +0100

    Created a django project and rendered the templates

---
+++ TechAidapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    '''Renders the homepage'''
+    return render (request,"index.html")
+
+def signin(request):
+    '''Renders the signinpage'''
+    return render (request,"signin.html")
+
+def login(request):
+    '''Renders the loginpage'''
+    return render (request,"login.html")
