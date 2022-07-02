0bd1cf4fb53ce777abc26749a2e846df71d715b4
Danielpraise <108065361+Danielpraise@users.noreply.github.com>
2022-07-02 01:25:41 +0100

    updated settings.py

--- TechAid/settings.py
+++ TechAid/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#gos9qiqq8jnmn&jnv#@#(@^*kx1gbu+xj_jzzd97c457($u-9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://techaid1.azurewebsites.net']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS =['static']
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
